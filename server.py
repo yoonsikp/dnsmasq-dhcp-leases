1394dec0fd799186e9f7504696345470bada9165
Yoonsik Park <park.yoonsik@icloud.com>
2018-07-15 05:54:33 -0400

    removed nbsp; changed to <pre> blocks

--- server.py
+++ server.py
@@ -5,9 +5,7 @@ import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 # Server settings
 # For port 80, which is normally used for a http server, you need root access
-PORT = 80
-
-
+PORT = 8081
 class HTTPRequestHandler(BaseHTTPRequestHandler):
     """
     Handles our HTTP requests
@@ -22,25 +20,25 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
         # Send message back to client
         lease_file = open('/var/lib/misc/dnsmasq.leases', 'r')
-        space = '&nbsp;'
-        message = '<!DOCTYPE html><html><body><h1>DHCP Leases</h1>\n<code>' + \
+        space = ' '
+        message = '<!DOCTYPE html><html><body><h1>DHCP Leases</h1>\n<pre><code>' + \
                   'Expiry' + space * 4 + \
                   'MAC Address' + space * 8 + \
                   'IP Address' + space * 7 + \
-                  'Hostname</br>'
-
+                  'Hostname\n'
         for line in lease_file.readlines():
             columns = line.split()
             columns[0] = time.strftime('%H:%M:%S',
                                        time.localtime(int(columns[0])))
             lease = ''
+            # discard last column
             for i in range(len(columns) - 1):
                 lease += columns[i] + space * 2
                 if i == 2:
                     lease += space * (15 - len(columns[2]))
-            message += '</br>\n' + lease
+            message += '\n' + lease
         lease_file.close()
-        message += '</code></body></html>'
+        message += '</code></pre></body></html>'
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
